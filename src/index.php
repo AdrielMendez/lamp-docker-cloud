09cfb14986525b7431d070e44009e038dd618761
AdrielMendez <adrielmauriciomendezreyessge@gmail.com>
2025-11-21 13:23:07 -0600

    Update index.php

--- src/index.php
+++ src/index.php
@@ -1,20 +1,22 @@
 <?php
-$host = "db4free.net";
-$user = "itca2025";
-$pass = "Itca123!";
-$db   = "proyectoitca";
+$host = getenv("DB_HOST");
+$user = getenv("DB_USER");
+$pass = getenv("DB_PASS");
+$db   = getenv("DB_NAME");
 
 $conn = new mysqli($host, $user, $pass, $db);
 
 if ($conn->connect_error) {
-    die("Error de conexión: " . $conn->connect_error);
+    die("❌ Error de conexión: " . $conn->connect_error);
 }
 
-echo "Conexión MySQL exitosa desde Render usando db4free.net!";
-?>
-
+echo "✅ Conexión MySQL exitosa en Render usando variables de entorno!";
 echo "LAMP con Docker en nube, proyecto ITCA FEPADE<br>";
 
 $result = $mysqli->query("SELECT NOW() AS fecha;");
 $row = $result->fetch_assoc();
 echo "Fecha MySQL: " . $row['fecha'];
+?>
+
+
+
